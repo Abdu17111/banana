3428848198e9889642de64fe686448cb70fa98dc
Abdu17111 <Abdufree245@gmail.com>
2025-02-14 18:37:16 +0600

    Add files via upload

---
+++ Новая папка (2)/Sss.js
@@ -0,0 +1,131 @@
+// let x = 'Пример строки'
+// let x1 = "Пример строки"
+// let x2 = 'Пример строки'
+
+// console.log(typeof x)
+// console.log(`i think ${y1} and ${y2} `)
+
+// const x1 = '10'
+// const x2 = 10
+// console.log(x1> x2)
+// console.log(x1 == x2)
+
+
+
+// alert ('ты попал на крутой сайт')
+// let res1 =prompt ('Сколько вам лет?','16')
+// let res2 =confirm ('ок да?')
+
+// Boolean
+// console.log (String (0))
+// consoLe.log (typeof "hello')
+// consoLe.log(BooLean (0))
+// console.log(typeof true)
+// consoLe.log (BooLean(''))
+// console.log(BooLean (' '))
+// consoLe.log(BooLean ( 'helLo'))
+
+// const res5 = 543 ** 3;
+// console.log(result);
+
+
+// const x1 ='cat'
+// console.log ===typ
+// const x 'cat'
+// if (typeof x === 'cat') {
+//     console.log("x является строкой");
+// } else {
+//     console.log("x не строка");
+// }
+// let arr1 = new Arrey(1,2,3,4,5)
+// console.log(arr1)
+
+
+
+
+// let arr = [1,2,3,4,5]
+// console.log(arr)
+
+// let x = ['h','e','l','0']
+// x.forEach(char => console.log(char + '!'))
+
+// let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 'a']
+// arr1.forEach(num => console.log(num))
+
+// let arr = [1, 2, 3]
+// let squared = 
+// arr.map
+// (num => num ** 2);
+// console.log(squared) 
+
+// =======
+
+// let x = ['hello','you','are','smart']  1 ДОМАШКА-СДЕЛАНО
+// x.forEach(char = console.log)
+
+// =======
+
+// let y  =  ['apple','bread','milk'] 2 ДОМАШКА- СДЕЛАНО
+// y.forEach(char => console.log(char + '-куплено'))
+
+// =======
+
+// let arr =[0,1,2,[3,4],5,6,[7,8,["you win!",10]]]  3  ДОМАШКА-СДЕЛАНО
+// console.log(arr[6][2][0])
+// let arr1 = [1, 2, 3, 4, 5, 6];
+
+// =======
+
+// let cub = arr1.map(num => num ** 3) 4 ДОМАШКА- СДЕЛАНО
+// console.log(cub)
+
+
+// function declaration
+
+// function hi() {
+//     console.log('hi')
+// }
+// hi
+
+
+// function summ(a,b,c)
+//     console.log(a + b + c)
+// }
+// summ(10,20,10)
+// summ(10,240,10)
+// summ(103,204,10)
+// summ(104,20,14)
+
+// function summ(a,b,s){
+//     if (a > b && a > s) {
+//         return a
+//     } else if (b > a && b > s) {
+//         return b
+//     } else {
+//         return s
+//     }
+// }
+// console.log(summ(122222,20,22))
+// function isMin(a, b, s, d) {
+//     return Math.min(a, b, s, d);
+// }
+// for (let i = 1; i <= 100; i++) {
+//     console.log("итерация номер -" +i)
+//     console.log("-")
+//     console.log("-")
+    
+// }
+// let x = 0;
+// for (let i = 0; i <= 46; i++) {
+//     if (i % 2 == 0) {
+//         x += i
+//         console.log(x);
+        
+//     }
+// } 
+// let i = 100
+// while (i > 0) {
+//     console.log(i)
+//     i--
+    
+// }
